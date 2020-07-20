1144e8e2f1913bfbbfa797d244886918c6bb15eb
afutured <afuture0@outlook.com>
2020-07-20 21:11:25 +0800

    init

---
+++ src/Countdown.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import FundClockProgress from "react-fundraising-countdown";
+
+class Countdown extends Component {
+  render() {
+    var d = new Date('Decmenber 19, 2020 08:00:00');
+    // const endOf2BusinessDays = bizDays(1010, d, getTimezoneName());
+    console.log(d);
+
+    return (
+      <div className="center-filp-clock">
+        <FundClockProgress campaignEndDate={d} />
+      </div>
+    );
+  }
+}
+export default Countdown;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Countdown from "./Countdown";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  return (
+    <div className="App">
+      <Countdown />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
